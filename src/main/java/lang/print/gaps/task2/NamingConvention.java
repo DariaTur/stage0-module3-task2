e4f8f3311bad8a899efbcd4efdcfc4f92638a4f1
Daria <dashatur100500@gmail.com>
2022-06-14 23:01:31 +0300

    add m3 t2

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int variable = 10;
+    	int variable = 10;
         int variable_one = 1;
         int variableTwo = 2;
         int variable_three = 3;
@@ -10,6 +10,6 @@ public class NamingConvention {
         int VARIABLE_FIVE = 5;
         int variableSix = 6;
         int variableSeven = 7;
-        int VariableEight = 8;
+        int variableEight = 8;
     }
 }
